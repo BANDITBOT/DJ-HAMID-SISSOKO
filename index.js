0a660b803d88d8de1d26c2e46c28acccf040e64c
BANDITBOT <45925719+BANDITBOT@users.noreply.github.com>
2018-12-17 02:31:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const cfg require('./index.json'); // a garder en version desktop
 const token = process.env.token;
 const prefix = ("?");
 
